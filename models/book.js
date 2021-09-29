4376c11d43b20e895dfd29df666bd953b90b3574
BozzleMay <ledley26@msn.com>
2021-09-29 17:52:43 +0100

    fileupload db

--- models/book.js
+++ models/book.js
@@ -26,7 +26,11 @@ createdAt: {
     required: true,
     default: Date.now
 },
-coverImageName: {
+coverImage: {
+    type: Buffer,
+    required: true
+},
+coverImageType: {
     type: String,
     required: true
 },
@@ -39,8 +43,8 @@ author: {
 
 })
 bookSchema.virtual('coverImagePath').get(function() {
-    if (this.coverImageName != null) {
-return path.join('/',  coverImageBasePath, this.coverImageName)
+    if (this.coverImage != null && this.coverImageType != null) {
+return `data: ${this.coverImageType};charset=utf-8;base64, ${this.coverImage.toString('base64')}`
     }
 })
 
